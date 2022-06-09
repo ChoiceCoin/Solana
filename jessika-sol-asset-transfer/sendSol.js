cce11b9438ce760aab8dcfd6122f6a8f0aecd122
AHAMUEFULE TERENCE <95743827+terrykeng@users.noreply.github.com>
2022-06-09 19:12:47 +0100

    Add files via upload

---
+++ jessika-sol-asset-transfer/sendSol.js
@@ -0,0 +1,43 @@
+const web3 =  require("@solana/web3.js");
+
+(async () => {
+  // Connect to cluster
+  console.log(web3.clusterApiUrl('devnet'))
+  const connection = new web3.Connection(
+    web3.clusterApiUrl('devnet'),
+    'confirmed',
+  );
+  // Uncomment the below command to test your connection to your node
+  //console.log(await connection.getEpochInfo())
+
+  // Using the secretkey to generate the keypair
+  const secretkey = Uint8Array.from()
+  const from = web3.Keypair.fromSecretKey(secretkey);  
+  
+  //If you already have enough test sol in your account you can comment this out
+  const airdropSignature = await connection.requestAirdrop(
+    from.publicKey,
+    web3.LAMPORTS_PER_SOL,
+  );
+  await connection.confirmTransaction(airdropSignature);
+
+
+  
+
+  // Add transfer instruction to transaction
+  const transaction = new web3.Transaction().add(
+    web3.SystemProgram.transfer({
+      fromPubkey: from.publicKey,
+      toPubkey: '5cG115Uv89fkAGMVSrLYBgnEX7MEiE7BJkd7pK9YdQJ4',
+      lamports: web3.LAMPORTS_PER_SOL / 100,
+    }),
+  );
+
+  // Sign transaction, broadcast, and confirm
+  const signature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [from],
+  );
+  console.log('SIGNATURE', signature);
+})();
